9d5c846546a8fd6b5c7090ba573cbc1102ae885f
AHanif09 <hanifahmad876@gmail.com>
2023-10-08 00:08:23 +0700

    styling css merapikan form

--- script.js
+++ script.js
@@ -13,13 +13,15 @@ function submit() {
     let hasilHitung = countData(getData())
     let hasil = hasilHitung.toFixed(2)
     if (hasil < 18.5) {
-        return (result.innerHTML = "Your BMI is " + hasil +" which means You are Underweight")
+        return (result.innerHTML = `Your BMI is <b>${hasil}</b> which means You are <b>Underweight</b>`)
     } else if (hasil >= 18.5 && hasil <= 24.9) {
-        return (result.innerHTML = "Your BMI is " + hasil +" which means You are Normal")
+        return (result.innerHTML = `Your BMI is <b>${hasil}</b> which means You are <b>Normal</b>`)
     } else if (hasil >= 25 && hasil <= 29.9) {
-        return (result.innerHTML = "Your BMI is " + hasil +" which means You are Overweight")
+        return (result.innerHTML = `Your BMI is <b>${hasil}</b> which means You are <b>Overwaight</b>`)
+    } else if (hasil >= 30) {
+        return (result.innerHTML = `Your BMI is <b>${hasil}</b> which means You are <b>Obesity</b>`)
     } else {
-        return (result.innerHTML = "Your BMI is " + hasil +" which means You are Obesity")
+        return (result.innerHTML = `<p style="color: red;"><b>Periksa kembali inputan</b></p>`) 
     }
 }
 
