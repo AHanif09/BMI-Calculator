bb201c49cbab56fd7b9ff7356c3b15f7efe8bf59
AHanif09 <hanifahmad876@gmail.com>
2023-10-08 12:40:34 +0700

    memperbaiki bug

--- script.js
+++ script.js
@@ -27,6 +27,6 @@ function submit() {
 
 let submitData = document.getElementById('submit')
     submitData.addEventListener("click", (event) => {
-    event.preventDefault();git
+    event.preventDefault();
     submit();
     });
